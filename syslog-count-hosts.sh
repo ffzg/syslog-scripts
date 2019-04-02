3cf0aabe2a038eabc2f10ab6a4875f55b3a30fa5
Dobrica Pavlinusic <dpavlin@rot13.org>
2019-04-02 10:06:57 +0200

    group by date and hour

--- syslog-count-hosts.sh
+++ syslog-count-hosts.sh
@@ -1,2 +1,3 @@
 #!/bin/sh -xe
-cat `test ! -z "$1" && echo $1 || echo /var/log/messages` | cut -d' ' -f4 | sort | uniq -c
+#cat `test ! -z "$1" && echo $1 || echo /var/log/messages` | cut -d' ' -f4 | sort | uniq -c
+cat `test ! -z "$1" && echo $1 || echo /var/log/messages` | cut -d' ' -f-5 | sed 's/:[0-9][0-9]:[0-9][0-9]/:__:__/' | sort | uniq -c
