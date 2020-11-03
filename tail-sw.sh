ad69ca46a26d529b21c5a81bad45ff6bd72ed7a2
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-11-03 10:11:45 +0100

    you can now specify -5000 to see last 5000 messages

--- tail-sw.sh
+++ tail-sw.sh
@@ -1,3 +1,8 @@
 #!/bin/sh -xe
 
-tail -f /var/log/syslog | egrep ' (wap|core|sw-|dox1x)'
+op='-f'
+test ! -z "$1" && op=$1
+
+tail $op /var/log/syslog \
+| egrep --line-buffered ' (wap|core|sw-|dox1x)' \
+| egrep --line-buffered -v '(core system user syslog logged|hostapd:)'
